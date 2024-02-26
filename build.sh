60b7155b5ddafe3b24d4d48d94869f8761af86bc
Vo Minh Thu <thu@hypered.io>
2024-02-26 23:31:29 +0100

    Remove the old Docker-based scripts.

--- build.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-STACK_IMAGE=${1:-7.8.4}
-
-docker run \
-  -v `pwd`/../minicron:/home/gusdev/minicron \
-  -v `pwd`:/home/gusdev/humming \
-  images.reesd.com/reesd/stack:$STACK_IMAGE \
-  cabal install minicron/minicron.cabal humming/humming.cabal
